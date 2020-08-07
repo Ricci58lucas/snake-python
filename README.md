23387420fd0f24ba27b25d72d5502517dfd6d5ad
ricci58Lucas <lucric2001@gmail.com>
2020-08-07 19:28:37 -0300

    updated README.md and added some minor features to code: refresh button after death and escape button to close game whenever

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # snake-python
-snake game with python. It will be using tkinter
+Snake game with python. It uses tkinter for graphic interface
+
+Known bugs:
+ -self.direction changes so fast that it doesn't give the snake time to update it's head coords. This may cause collision problems
--- main.py
+++ main.py
@@ -2,45 +2,50 @@ import tkinter as tk
 from PIL import Image, ImageTk
 from random import randint
 
-# -- frame, canvas and graphics configuration -- #
+# -- graphics configuration -- #
 BG_COLOR = "#577757"
 
 # its half should always be divisible by 20
 WIN_WIDTH = 600
 WIN_HEIGHT = 640
 
-# -- game configuration -- #
+# -- game mechanics configuration -- #
 
-MOVES_PER_SECOND = 15
+MOVES_PER_SECOND = 17
 GAME_SPEED = 1000 // MOVES_PER_SECOND
 
 # amount of pixels snake advances when it moves
-# it should always be 20, as each image is 20x20 px
+# should always be 20, as each image is 20x20 px
 MOVE_INCREMENT = 20
 
 # create our own canvas class (inheriting from tk.Canvas class)
 class Snake(tk.Canvas):
-    # load canvas pre-config (like a constructor)
     def __init__(self):
         super().__init__(width=WIN_WIDTH, height=WIN_HEIGHT, background=BG_COLOR, highlightthickness=0)
+
+        self.game_ended = False
 
         # set starting snake_body and food positions
         self.snake_body_pos = [(WIN_WIDTH/2, WIN_HEIGHT/2)]
         self.food_pos = self.pick_new_food_pos()
 
-        # set starting score and direction
         self.score = 0
-        self.direction = "Right"
 
-        # listen to keybord, execute self.key_press when key pressed
+        # all posible directions and all posible opposites
+        self.all_direction = ("Up", "Down", "Left", "Right")
+        self.opposites = ({"Up", "Down"}, {"Left", "Right"})
+        
+        self.direction = ""
+
+        # call function self.key_press when any key is pressed
         self.bind_all("<Key>", self.key_press)
 
         # import and load snake_body and food images
         self.load_assets()
-        # put objects (snake_body and food) in canvas
+        # draw objects in canvas
         self.draw()
 
-        # wait [GAME_SPEED]ms and execute self.update
+        # start loop
         self.after(GAME_SPEED, self.update)
 
     def load_assets(self):
@@ -55,67 +60,76 @@ class Snake(tk.Canvas):
             root.destroy()
 
     def draw(self):
-        # load snake and food images and "tag" every image object
+        # draw snake and food images and nametag them
         for x_pos, y_pos in self.snake_body_pos:
-            self.create_image(x_pos, y_pos, image=self.snake_body, tag="snake")
+            self.create_image(x_pos, y_pos, image = self.snake_body, tag = "snake")
 
-        self.create_image(*self.food_pos, image=self.food, tag="food")
+        self.create_image(*self.food_pos, image = self.food, tag = "food")
 
         # draw border line
         self.create_rectangle(
             MOVE_INCREMENT/2, 
             MOVE_INCREMENT/2, 
-            WIN_WIDTH-(MOVE_INCREMENT/2), 
-            WIN_HEIGHT-(MOVE_INCREMENT/2), 
-            outline="#000000"
+            WIN_WIDTH - (MOVE_INCREMENT/2), 
+            WIN_HEIGHT - (MOVE_INCREMENT/2), 
+            outline = "#000000"
         )
 
     def update(self):
         if self.check_collition():
             self.end_game()
-            return
 
-        # check if snake ate
-        self.eat()
+        if self.snake_body_pos[0] == self.food_pos:
+            self.eat()
 
-        # call move snake and then call perform_actions after 75ms
-        self.move()
+        if self.direction in self.all_direction:
+            self.move()
+
         self.after(GAME_SPEED, self.update)
 
     def check_collition(self):
         head_x_pos, head_y_pos = self.snake_body_pos[0]
-
-        # return True if any is true
         return (
-            # check if head touched borders
-            head_x_pos in (0, WIN_WIDTH)
-            or
-            head_y_pos in (0, WIN_HEIGHT)
-            or
-            # check if head touched its body
+            head_x_pos in (0, WIN_WIDTH) or 
+            head_y_pos in (0, WIN_HEIGHT) or 
             (head_x_pos, head_y_pos) in self.snake_body_pos[1:]
         )
 
     def eat(self):
-        if self.snake_body_pos[0] == self.food_pos:
-            # duplicate the last element of the snake (then remove it when self.move())
-            self.snake_body_pos.append(self.snake_body_pos[-1])
-            
-            # create image of last position
-            self.create_image(*self.snake_body_pos[-1], image=self.snake_body, tag="snake")
+        # duplicate the last element of the snake (then remove it when self.move())
+        self.snake_body_pos.append(self.snake_body_pos[-1])
+                
+        # create image of last position
+        self.create_image(*self.snake_body_pos[-1], image=self.snake_body, tag="snake")
 
-            # assing new random position to food
-            self.food_pos = self.pick_new_food_pos()
-            self.coords(self.find_withtag("food"), self.food_pos)
+        self.food_pos = self.pick_new_food_pos()
+        self.coords(self.find_withtag("food"), self.food_pos)
 
-            # update score value
-            self.score += 1
+        self.score += 1
 
+    def key_press(self, e):
+        # get binded symbol of pressed key
+        key_pressed = e.keysym
+
+        if key_pressed in self.all_direction:
+            new_direction = key_pressed
+
+            # check if the direction is valid and if new direction is not opposite to current
+            if {new_direction, self.direction} not in self.opposites:
+                self.direction = new_direction
+        elif key_pressed == "Escape":
+            # close window
+            root.destroy()
+        elif key_pressed == "space" and self.game_ended:
+            # refresh game
+            self.destroy()
+            screen = Snake()
+            screen.pack()
+            root.mainloop()
+      
     def move(self):
-        # obtain current head position
         head_x_pos, head_y_pos = self.snake_body_pos[0]
        
-        # check direction and set movement
         if self.direction == "Up":
             new_head_pos = (head_x_pos, head_y_pos - MOVE_INCREMENT)
         elif self.direction == "Down":
@@ -125,48 +139,36 @@ class Snake(tk.Canvas):
         elif self.direction == "Right":
             new_head_pos = (head_x_pos + MOVE_INCREMENT, head_y_pos)
         
-        # update head and body positions in snake body array
+        # update head and body positions in snake_body array
         self.snake_body_pos = [new_head_pos] + self.snake_body_pos[:-1]
 
-        # move every image object into its new position (update drawing)
+        # update drawing. move every image object into its new position
         for element, position in zip(self.find_withtag("snake"), self.snake_body_pos):
-            # moves "element" into "position"
             self.coords(element, position)
-
-    def key_press(self, e):
-        # get key press symbol ("Up", "Down", "Left", "Right")
-        new_direction = e.keysym
-
-        al_direction = ("Up", "Down", "Left", "Right")
-        opposites = ({"Up", "Down"}, {"Left", "Right"})
-            
-        # check if the direction is valid and if new direction is nor opposite to current
-        if (new_direction in al_direction) and {new_direction, self.direction} not in opposites:
-                self.direction = new_direction
 
     def pick_new_food_pos(self):
         while True:
-            # pick new random position
             x_pos = randint(1, (WIN_WIDTH-MOVE_INCREMENT)/MOVE_INCREMENT) * MOVE_INCREMENT
             y_pos = randint(1, (WIN_HEIGHT-MOVE_INCREMENT)/MOVE_INCREMENT) * MOVE_INCREMENT
-            # set new position
             food_pos = (x_pos, y_pos)
 
-            #check if position is inside snake
+            # prevent food from spawning inside snake
             if food_pos not in self.snake_body_pos:
                 return food_pos
 
     def end_game(self):
-        # erase every object in canvas and show final score
+        self.game_ended = True
+
+        # deletes canvas content
         self.delete(tk.ALL)
         self.create_text(
             WIN_WIDTH/2,
-            WIN_HEIGHT/2, 
-            text=f"Final Score: {self.score}",
-            fill="#000000",
-            font=("tkDefaultFont", 24)
+            WIN_HEIGHT/2,
+            text = f"Game Over! Final Score: {self.score}\nPress space to Start Over",
+            fill = "#000000",
+            font = ("TkDefaultFont", 24)
         )
-
+      
 root = tk.Tk()
 root.title("Snake Game")
 root.resizable(False, False)
